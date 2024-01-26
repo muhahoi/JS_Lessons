c7e16a0586c39074320a4493ee7e46dcb921c03e
mahout <mahout.fe@gmail.com>
2024-01-26 10:08:59 +0300

    закончил практику 4

--- film_catalog/JS/script.js
+++ film_catalog/JS/script.js
@@ -47,41 +47,23 @@ const personalMovieDB = {
 			console.log(personalMovieDB);
 		}
 	},
-	writeYourGenres: function () {
-		for (let i = 0; i < 3; i++) {
-			personalMovieDB.genres[i] = prompt(
-				`Ваш любимый жанр под номером ${i + 1}`,
-				''
-			);
-			if (
-				personalMovieDB.genres[i] === '' ||
-				personalMovieDB.genres[i] === null
-			) {
-				i--;
-			}
-		}
-		personalMovieDB.genres.forEach((genres, i) =>
-			console.log(`Любимый жанр ${i + 1} - это ${genres}`)
-		);
-	},
 	toggleVisibleMyDB: function () {
 		personalMovieDB.privat
 			? (personalMovieDB.privat = false)
 			: (personalMovieDB.privat = true);
 	},
+	writeYourGenres: function () {
+		for (let i = 1; i <= 3; i++) {
+			let genre = prompt(`Ваш любимый жанр под номером ${i}`);
+			if (genre === '' || genre === null) {
+				console.log('Вы ввели некоректные данные или не ввели их вовсе');
+				i--;
+			} else {
+				personalMovieDB.genres[i - 1] = genre;
+			}
+		}
+		personalMovieDB.genres.forEach((genre, index) =>
+			console.log(`Любимый жанр ${index + 1} - это ${genre}`)
+		);
+	},
 };
-
-/* Задание на урок:
-
-1) У нас уже есть рабочее приложение, состоящее из отдельных функций. Представьте, что
-перед вами стоит задача переписать его так, чтобы все функции стали методами объекта personalMovieDB
-Такое случается в реальных продуктах при смене технологий или подхода к архитектуре программы
-
-2) Создать метод toggleVisibleMyDB, который при вызове будет проверять свойство privat. Если оно false - он
-переключает его в true, если true - переключает в false. Протестировать вместе с showMyDB.
-
-3) В методе writeYourGenres запретить пользователю нажать кнопку "отмена" или оставлять пустую строку. 
-Если он это сделал - возвращать его к этому же вопросу. После того, как все жанры введены - 
-при помощи метода forEach вывести в консоль сообщения в таком виде:
-"Любимый жанр #(номер по порядку, начиная с 1) - это (название из массива)"*/
-personalMovieDB.start();
