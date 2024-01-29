962ac50254782bfdff9e22347a073266178394a0
mahout <93513481+muhahoi@users.noreply.github.com>
2024-01-29 08:51:17 +0300

    начал задачу 15

--- javascript_tasks/JS/script.js
+++ javascript_tasks/JS/script.js
@@ -837,7 +837,7 @@
 // a = [1, 2, 3]; b = [1, 2, 3]; Правда ли что a == b ? //нет
 // Что выведет этот код: alert( +'Infinity'); Infinity
 // Верно ли сравнение: 'Ёжик' > 'яблоко'? // false
-// Чему равно 0 || '' || 2 || undefined || true || falsе ? //2
+// Чему равно 0 || '' || 2 || undefined || true || falsе ? //true
 
 //**[] + false - null + true;
 
@@ -853,22 +853,94 @@
 
 //**alert('1'[0]) */
 
-console.log('1'[0]); //'1'
-console.log('Привет'[3]); //'в'
+//console.log('1'[0]); //'1'
+//console.log('Привет'[3]); //'в'
 
 //**2 && 1 && null && 0 && undefined */
 
-console.log(2 && 1 && null && 0 && undefined); //null
+//console.log(2 && 1 && null && 0 && undefined); //null
 
 //** !!( a && b ) и (a && b) */
 
-console.log(!!(1 && 2)); //true;
-console.log(1 && 2); //2;
+//console.log(!!(1 && 2)); //true;
+//console.log(1 && 2); //2;
 
 //**alert( null || 2 && 3 || 4 ) */
 
-console.log(null || (2 && 3) || 4); //3
+//console.log(null || (2 && 3) || 4); //3
 
 //**alert( +'Infinity') */
 
-console.log(+'Infinity'); // Infinity
+//console.log(+'Infinity'); // Infinity
+
+//** Упражнение по написанию кода 15: Задания на поиск ошибок в коде */
+// 1) Функция isOpen не хочет правильно работать. Что мы уже не пробовали подставлять в неё - результат все время неправильный. Необходимо найти причины и исправить.
+
+// 2) Функция isAverageLunchPriceTrue должна брать цены двух любых блюд из меню, складывать их и сравнивать с средним чеком (averageLunchPrice).
+
+// Сейчас функция работает, но постоянно выдает неправильный результат. Ведь из представленного меню сумма двух любых цен всегда будет больше 20. Необходимо найти причину и исправить.
+
+// 3) Функция transferWaitors создана для того, чтобы копировать шаблон данных и передавать их в другой ресторан. Конечно, в другом ресторане будут другие блюда, другие официанты и тп. Сейчас эта функция только в начале разработки и должна менять данные про официантов.
+
+// Но в нынешнем виде мы обнаружили, что после её запуска не только копия данных содержит новых официантов, но и основные данные! В restorantData сотрудник Alice исчезает и заменяется Mike! Необходимо найти причину и немедленно исправить, чтобы данные были разделены.
+
+const restorantData = {
+	menu: [
+		{
+			name: 'Salad Caesar',
+			price: '14$',
+		},
+		{
+			name: 'Pizza Diavola',
+			price: '9$',
+		},
+		{
+			name: 'Beefsteak',
+			price: '17$',
+		},
+		{
+			name: 'Napoleon',
+			price: '7$',
+		},
+	],
+	waitors: [
+		{ name: 'Alice', age: 22 },
+		{ name: 'John', age: 24 },
+	],
+	averageLunchPrice: '20$',
+	openNow: true,
+};
+
+function isOpen(prop) {
+	let answer = '';
+	prop ? (answer = 'Открыто') : (answer = 'Закрыто');
+
+	return answer;
+}
+
+console.log(isOpen(restorantData.openNow));
+
+function isAverageLunchPriceTrue(fDish, sDish, average) {
+	if (+fDish.price.slice(0, -1) + sDish.price < average) {
+		return 'Цена ниже средней';
+	} else {
+		return 'Цена выше средней';
+	}
+}
+
+console.log(
+	isAverageLunchPriceTrue(
+		restorantData.menu[0],
+		restorantData.menu[1],
+		restorantData.averageLunchPrice
+	)
+);
+
+function transferWaitors(data) {
+	const copy = Object.assign({}, data);
+
+	copy.waitors[0] = { name: 'Mike', age: 32 };
+	return copy;
+}
+
+transferWaitors(restorantData);
