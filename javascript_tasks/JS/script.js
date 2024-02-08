9e2624954d894c1f60ba6564c5090995abcbef95
mahout <93513481+muhahoi@users.noreply.github.com>
2024-02-09 01:41:17 +0300

    закончил задачу 19

--- javascript_tasks/JS/script.js
+++ javascript_tasks/JS/script.js
@@ -1148,9 +1148,23 @@
 
 //Напишите функцию isPangram, которая принимает в себя строку и возвращает логическое значение. Если строка является панграммой - вернется true, если нет - false.
 
-function isPangram(string) {
-	let strWithoutSpaces = string.split(' ').join('');
-	return Array.from(new Set(strWithoutSpaces.toLowerCase())).length == 26;
-}
+//function isPangram(string) {
+//	let strWithoutSpaces = string.split(' ').join('');
+//	return Array.from(new Set(strWithoutSpaces.toLowerCase())).length == 26;
+//}
 
-console.log(isPangram('The quick brown fox jumps over the lazy dog'));
+//console.log(isPangram('The quick brown fox jumps over the lazy dog'));
+
+//* Упражнение по написанию кода 19: (**) Сложная задача на работу с рекурсией
+
+//Создайте функцию deepCount, которая будет считать количество всех элементов в массиве, включая и вложенные массивы. Учтите, что сам вложенный массив тоже входит в счет. Чтобы понять задачу детальнее, давайте рассмотрим примеры:
+
+//deepCount([1, 5, 3]) => 3
+
+//deepCount(["1", 5, "3", ["10"]]) => 5 (Заметьте, что последний элемент был посчитан сам + его внутренность)
+
+//deepCount([1, 2, [3, 4, [5]]]) => 7
+
+//deepCount([]) => 0
+
+//deepCount([[[[[[[[[]]]]]]]]]) => 8
--- javascript_tasks/JS/test.js
+++ javascript_tasks/JS/test.js
@@ -1,9 +1,24 @@
-function powqa(x, n) {
-	if (n === 1) {
-		return x;
+//function powqa(x, n) {
+//	if (n === 1) {
+//		return x;
+//	} else {
+//		return x * powqa(x, n - 1);
+//	}
+//}
+
+//console.log(powqa(2, 4));
+
+function deepCount(a) {
+	if (!Array.isArray(a) || !a.some(elem => Array.isArray(elem))) {
+		return a.length;
 	} else {
-		return x * powqa(x, n - 1);
+		return a.reduce(function (acc, val) {
+			return acc + (Array.isArray(val) ? deepCount(val) + 1 : 1);
+		}, 0);
 	}
 }
-
-console.log(powqa(2, 4));
+console.log(deepCount([1, 5, 3])); // 3
+console.log(deepCount(['1', 5, '3', ['10']])); // 5
+console.log(deepCount([1, 2, [3, 4, [5]]])); // 7
+console.log(deepCount([])); // 0
+console.log(deepCount([[[[[[[[[]]]]]]]]])); // 8
