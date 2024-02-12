15d46917830e786ce005bddfe84a2e0144d33024
mahout <93513481+muhahoi@users.noreply.github.com>
2024-02-13 00:29:32 +0300

    шпоры

--- classList/script.js
+++ classList/script.js
@@ -1,17 +1,18 @@
 const btns = document.querySelectorAll('button'),
 	wrapper = document.querySelector('.btn-block');
-//console.log(btns[0].classList.length); // 2 у первой кнопки два класса
-//console.log(btns[0].classList.item(0)); // blue
-//console.log(btns[1].classList.add('red')); // у первой кнопки появится класс red
-//console.log(btns[0].classList.add('red', 'white')); // можно добавить несколько классов
-//console.log(btns[0].classList.remove('blue')); // удаляет у первой кнопки класс blue
-//console.log(btns[0].classList.toggle('blue')); // если класса нет, то он появится и наоборот
+console.log(btns[0].classList.length); // 2 у первой кнопки два класса
+console.log(btns[0].classList.item(0)); // blue
+console.log(btns[1].classList.add('red')); // у первой кнопки появится класс red
+console.log(btns[0].classList.add('red', 'white')); // можно добавить несколько классов
+console.log(btns[0].classList.remove('blue')); // удаляет у первой кнопки класс blue
+console.log(btns[0].classList.toggle('blue')); // если класса нет, то он появится и наоборот
 
 //* проверяем есть ли класс или нет
 
-//if (btns[1].classList.contains('red')) { // проверяет есть ли такой класс и возвращает булиновое значение
-//	console.log('red');
-//}
+if (btns[1].classList.contains('red')) {
+	// проверяет есть ли такой класс и возвращает булиновое значение
+	console.log('red');
+}
 
 //* кнопка будет менять цвет по клику
 btns[0].addEventListener('click', () => {
