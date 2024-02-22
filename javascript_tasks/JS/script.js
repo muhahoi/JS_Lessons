01bc4a30768f8f440193152aff9c50e1f6aec63a
mahout <93513481+muhahoi@users.noreply.github.com>
2024-02-22 23:32:13 +0300

    задача

--- javascript_tasks/JS/script.js
+++ javascript_tasks/JS/script.js
@@ -1182,3 +1182,42 @@
 //console.log(deepCount([1, 2, [3, 4, [5]]])); // 7
 //console.log(deepCount([])); // 0
 //console.log(deepCount([[[[[[[[[]]]]]]]]])); // 8
+
+//* задача
+//* отсортировать массив
+
+//let array = [
+//	'John',
+//	34,
+//	'John',
+//	'jOhin',
+//	'55',
+//	55,
+//	'55',
+//	55,
+//	'55',
+//	41,
+//	1998,
+//	199.5,
+//	'toUpperCase',
+//	55,
+//	'Ivan',
+//	1998,
+//];
+
+//const uniq = array.filter((item, index) => array.indexOf(item) === index);
+
+//let uniqArr = [];
+//for (let i = 0; i < uniq.length; i++) {
+//	if (
+//		typeof uniq[i] === 'string' &&
+//		uniq.includes(uniq[i]) &&
+//		uniq.includes(parseInt(uniq[i]))
+//	)
+//		continue;
+//	{
+//		uniqArr.push(uniq[i]);
+//	}
+//}
+
+//console.log(uniqArr);
