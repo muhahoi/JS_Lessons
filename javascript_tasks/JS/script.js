300fd311901b56d3d8a9b401ee7bf55a3ef3efdc
mahout <93513481+muhahoi@users.noreply.github.com>
2024-03-08 23:17:18 +0300

    решил 21 задачу

--- javascript_tasks/JS/script.js
+++ javascript_tasks/JS/script.js
@@ -1383,53 +1383,90 @@
 
 // 4) Запишите результат предыдущей функции в переменную tranformedArray. Напишите функцию checkFilms, которая будет проверять, что в каждом из фильмов есть поле id. Если это так - функция возвращает true. Очевидно, что сейчас условие должно выполняться, если мы передаем checkFilms(tranformedArray); :)
 
-const films = [
-	{
-		name: 'Titanic',
-		rating: 9,
-	},
-	{
-		name: 'Die hard 5',
-		rating: 5,
-	},
-	{
-		name: 'Matrix',
-		rating: 8,
-	},
-	{
-		name: 'Some bad film',
-		rating: 4,
-	},
+//const films = [
+//	{
+//		name: 'Titanic',
+//		rating: 9,
+//	},
+//	{
+//		name: 'Die hard 5',
+//		rating: 5,
+//	},
+//	{
+//		name: 'Matrix',
+//		rating: 8,
+//	},
+//	{
+//		name: 'Some bad film',
+//		rating: 4,
+//	},
+//];
+
+//function showGoodFilms(arr) {
+//	return arr.filter(item => item.rating >= 8);
+//}
+
+//function showListOfFilms(arr) {
+//	return arr
+//		.map(obj => Object.entries(obj))
+//		.map(item => item[0][1])
+//		.join(', ');
+//}
+
+//function setFilmsIds(arr) {
+//	// мой вариант
+//	let result = structuredClone(arr);
+//	for (let i = 0; i < result.length; i++) {
+//		result[i]['id'] = i;
+//	}
+//	return result;
+
+//	// вариант Ивана
+//	//return arr.map((film, i) => {
+//	//	film.id = i;
+//	//	return film;
+//}
+
+//const tranformedArray = setFilmsIds(films);
+
+//function checkFilms(arr) {
+//	return arr.every(obj => 'id' in obj);
+//}
+//console.log(checkFilms(films));
+
+//* Задача 21
+
+//1) У вас есть небольшой массив с данными о доходах каждой торговой точки. Напишите функцию getPositiveIncomeAmount, которая принимает этот массив данных и возвращает сумму только положительных значений из каждого объекта. (число)
+
+//Пример:
+
+//getPositiveIncomeAmount(funds) => 13300
+
+//2) Напишите функцию getTotalIncomeAmount, которая тоже принимает этот массив данных. Если хотя бы один из объектов содержит отрицательное значение поля amount, то функция возвращает сумму всех значений. (число) Если таких значений нет - запускается функция getPositiveIncomeAmount с тем же массивом данных.
+
+//Пример:
+
+//getTotalIncomeAmount(funds) => -500
+
+const funds = [
+	{ amount: -1400 },
+	{ amount: 2400 },
+	{ amount: -1000 },
+	{ amount: 500 },
+	{ amount: 10400 },
+	{ amount: -11400 },
 ];
 
-function showGoodFilms(arr) {
-	return arr.filter(item => item.rating >= 8);
-}
+const getPositiveIncomeAmount = data => {
+	return data.reduce(
+		(acc, curr) => (curr.amount > 0 ? acc + curr.amount : acc),
+		0
+	);
+};
 
-function showListOfFilms(arr) {
-	return arr
-		.map(obj => Object.entries(obj))
-		.map(item => item[0][1])
-		.join(', ');
-}
-
-function setFilmsIds(arr) {
-	// мой вариант
-	let result = structuredClone(arr);
-	for (let i = 0; i < result.length; i++) {
-		result[i]['id'] = i;
-	}
-	return result;
-
-	// вариант Ивана
-	//return arr.map((film, i) => {
-	//	film.id = i;
-	//	return film;
-}
-
-const tranformedArray = setFilmsIds(films);
-
-function checkFilms(arr) {
-	return arr.every(obj => 'id' in obj);
-}
-console.log(checkFilms(films));
+const getTotalIncomeAmount = data => {
+	return data.some(item => item.amount > 0)
+		? data.reduce((acc, curr) => acc + curr.amount, 0)
+		: getPositiveIncomeAmount(data);
+};
+console.log(getTotalIncomeAmount(funds));
