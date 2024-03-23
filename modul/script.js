f2c9b3a2d165b70a1426abf07b6556e345b0cf3b
mahout <93513481+muhahoi@users.noreply.github.com>
2024-03-23 22:30:54 +0300

    modul

---
+++ modul/script.js
@@ -0,0 +1,23 @@
+'use strict';
+
+//* метод "модуль"
+const number = 1;
+(function () {
+	let number = 2;
+	console.log(number); //2
+	console.log(number + 3); // 5
+})();
+
+console.log(number); // 1
+
+const user = (function () {
+	const privat = function () {
+		console.log('I am privat');
+	};
+
+	return {
+		sayHello: privat,
+	};
+})();
+
+user.sayHello(); // I am privat
