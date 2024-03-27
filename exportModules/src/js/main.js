7adc844c46488c0d0417458b16522ad9cf60f33e
mahout <93513481+muhahoi@users.noreply.github.com>
2024-03-27 23:12:39 +0300

    import и export

---
+++ exportModules/src/js/main.js
@@ -0,0 +1,12 @@
+export let one = 1;
+
+let two = 2;
+
+export { two };
+
+export function sayHi() {
+	console.log('Hello');
+}
+export default function sayBuy() {
+	console.log('Buy');
+}
---
+++ exportModules/src/js/script.js
@@ -0,0 +1,9 @@
+import { one, two } from './main.js';
+
+import { sayHi } from './main.js';
+
+import sayBuy from './main.js';
+
+console.log(`${one} and ${two}`);
+sayHi();
+sayBuy();
